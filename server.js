8316fe82b570c66278773cd7b064f07f30b797dc
Chen YE <yechen35@outlook.com>
2026-05-16 18:18:58 +0800

    fix: add UTF-8 charset to JSON responses

--- server.js
+++ server.js
@@ -84,6 +84,10 @@ app.use(cors({
   }
 }));
 app.use(express.json({ limit: '1mb' }));
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Database
